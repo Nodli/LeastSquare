68cbd991d46364a02371c7c3edb2adcd61e25a00
Nodli <rambure.hugo@hotmail.fr>
2017-11-18 17:53:54 +0100

    Implemented the Regression2D class

--- include/regression.h
+++ include/regression.h
@@ -1,18 +1,40 @@
 #ifndef REG_H
 #define REG_H
 
-class Regression{
+/**
+* \class Provides easy access to 2D regression functions
+*/
+class Regression2D{
 public:
-	Regression(int size, double* valuesX, double* valuesY);
-	~Regression();
+	/**
+	* \brief Constructor of a Regression object
+	* \details The Regression instance holds copies of valuesX and valuesY
+	* \param[in] s Number of data points to use for the regression
+	* \param[in] valuesX X-Axis values of the data points
+	* \param[in] valuesY Y-Axis values of the data points
+	*/
+	Regression2D(int s, const double* valuesX, const double* valuesY);
+	/**
+	* \brief Destructor of a Regression object
+	* \details Deletes the dynamically allocated valuesX_ and valuesY_
+	*/
+	~Regression2D();
 
-	double regLin();
-	double regParab();
-	
+	/**
+	* \brief Computes a linear regression using the data points
+	* \details Displays the (a, b) coefficients for the Y = aX + b equation
+	*/
+	void Linear() const;
+	/**
+	* \brief Computes a parabolic regresiion using the data points
+	* \details Displays the (a, b, c) coefficients for the Y = aX^2 + bX + c equation
+	*/
+	void Parabolic() const;
+
 private:
-	double size_;
-	double* valuesX_;
-	double* valuesY_;
+	int size_; //Raw size of the Matrix
+	double* valuesX_; //Points towards the X-Axis value table of the data points
+	double* valuesY_; //Points towards the Y-Axis value table of the data points
 };
 
 #endif
---
+++ src/regression.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <limits>
+#include <cassert>
+#include <regression.h>
+#include <matrix.h>
+
+Regression2D::Regression2D(int s, const double* valuesX, const double* valuesY): size_(s){
+	std::cout << "Regression2D(int, double*, double*) at " << this << std::endl;
+	valuesX_ = new double[size_];
+	valuesY_ = new double[size_];
+	for(int i = 0; i != size_; ++i){
+		valuesX_[i] = valuesX[i];
+		valuesY_[i] = valuesY[i];
+	}
+}
+
+Regression2D::~Regression2D(){
+	std::cout << "~Regression2D at " << this << std::endl;
+	delete[] valuesX_;
+	delete[] valuesY_;
+}
+
+void Regression2D::Linear() const{
+	std::cout << "Linear() at " << this << std::endl;
+	double avalues[4] = {0, 0, 0, 0};
+	double bvalues[2] = {0, 0};
+	for(int i = 0; i != size_; ++i){
+		avalues[0] += valuesX_[i] * valuesX_[i];
+		avalues[1] += valuesX_[i];
+		avalues[2] += valuesX_[i];
+		bvalues[0] += valuesX_[i] * valuesY_[i];
+		bvalues[1] += valuesY_[i];
+	}
+	avalues[3] = size_;
+	Matrix a(2, 2, avalues);
+	Matrix b(2, 1, bvalues);
+	a.inv22() *= b;
+	std::cout << "(a, b) = (" << a.getValue(0) << ", " << a.getValue(1) << ")" << std::endl;
+}
+
+void Regression2D::Parabolic() const{
+	std::cout << "Parabolic() at " << this << std::endl;
+	double bvalues[3] = {0, 0, 0};
+	double x4sum = 0;
+	double x3sum = 0;
+	double x2sum = 0;
+	double xsum = 0;
+	for(int i = 0; i != size_; ++i){
+		x4sum += valuesX_[i] * valuesX_[i] * valuesX_[i] * valuesX_[i];
+		x3sum += valuesX_[i] * valuesX_[i] * valuesX_[i];
+		x2sum += valuesX_[i] * valuesX_[i];
+		xsum += valuesX_[i];
+		bvalues[0] += valuesX_[i] * valuesX_[i] * valuesY_[i];
+		bvalues[1] += valuesX_[i] * valuesY_[i];
+		bvalues[2] += valuesY_[i];
+	}
+	double avalues[9] = {x4sum, x3sum, x2sum, x3sum, x2sum, xsum, x2sum, xsum, size_};
+	Matrix a(3, 3, avalues);
+    Matrix b(3, 1, bvalues);
+    a.inv33();
+    a *= b;
+    std::cout << "(a, b, c) = (" << a.getValue(0) << ", " << a.getValue(1) << ", " << a.getValue(2) << ")" << std::endl;
+}
